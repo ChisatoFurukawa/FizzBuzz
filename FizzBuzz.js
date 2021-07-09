564df7b1173d2240e3813bdd6281a3d4311dd508
ChisatoFurukawa <86587691+ChisatoFurukawa@users.noreply.github.com>
2021-07-09 10:37:10 +0900

    HTML使用

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -5,7 +5,33 @@ for(const ans = 1; ans<=100; ans++){
     else if(ans % 3 == 0){
         console.log("Fizz");
     }
-    else if(ans % 5 == 0){
+    else if(ans % 5 == 0){    const indicate =document.getElementById("indicate");
+    const result = document.getElementById("result");
+    function writeResult(ele){
+     indicate.value = indicate.value+ele.value
+    }
+    function calc(){
+    if(indicate.value == ""){
+            return
+        }
+        result.value = new function(return);
+    }
+    for(const indicate = 1; indicate<=100; indicate++){
+    if(indicate.value = indicate % 3 == 0 && indicate % 5 == 0){
+       result.value = "FizzBuzz";
+    }
+    else if(indicate % 3 == 0){
+       resulr.value = "Fizz";
+    }
+    else if(indicate % 5 == 0){
+        resulr.value = "Buzz";
+    }else{
+        indicate
+    }
+    paragraph.innerText = 'result';
+
+}
+  
         console.log("Buzz");
     }
     else{
